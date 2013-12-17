1adee24e71b2f7639c7e122c934a52ec3ff7442d
CHUA Chee Wee <github@chuacw.ath.cx>
2013-12-17 16:52:06 +0800

    Initial commit

---
+++ hexToLongOperation.java
@@ -0,0 +1,17 @@
+/*
+ *
+
+Portion of custom ArcSight FlexConnector developed by chuacw@gmail.com
+
+
+ */
+package REDACTED;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class hexToLongOperation {
+
+
+}
